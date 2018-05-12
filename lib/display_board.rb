11ca2c8777ca31b0a3dfc282206d33820b37d387
Petro Tarnavskyy <petro.tarnavskyy@lnu.edu.ua>
2018-05-12 14:31:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
 board=[]
 9.times {board<<" "}
-def display_board
 ind=0
   3.times {
     puts "#{board[ind]} | #{board[ind+1]} | #{board[ind+2]}"
